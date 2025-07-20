efcef89b23d0cb5325c8cecb53fe87a57cd1ffbe
Aleks <test_spb@list.ru>
2025-07-20 11:30:22 +0300

    postman-echo-test

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,18 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
