a5a34d2d1543a2402e59e6feff018aa683f9bbb4
Aleks <test_spb@list.ru>
2025-07-20 11:58:55 +0300

    postman-echo-test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,18 +1,41 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
+
     @Test
-    void shouldReturnSentData() {
+    public void postRequest_shouldReturnSentData() {
+        String sentData = "some data";
+
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .contentType("text/plain; charset=UTF-8")
+                .body(sentData)
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                // Проверяем, что в поле "data" содержится отправленная строка
+                .body("data", equalTo(sentData));
+    }
+
+    //@Test
+    public void postRequest_withWrongJsonPath_shouldFail() {
+        String sentData = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(sentData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                // Неверное поле - тест должен упасть
+                .body("wrongField", equalTo(sentData));
     }
 }
+
